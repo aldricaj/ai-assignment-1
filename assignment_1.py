ead20648898276bad532facbc6eb691f65ba9f1f
Andrew Aldrich <aldricaj@mail.uc.edu>
2019-09-24 19:19:54 -0400

    checkpoint

--- assignment_1.py
+++ assignment_1.py
@@ -1,16 +1,43 @@
 import sys
 
+import models.board as models
+
 def parse_input():
     start_state = None
     end_state = None
-    if len(sys.argv > 3):
+    if len(sys.argv) > 3:
         # parse input
-        start_state = ''
-        end_state = ''
+        start_state = []
+        end_state = []
         for s in sys.argv[1:]:
-            if start_state[-1] != ']':
-                start_state.push
+            print(s)
+            if s == ' ':
+                continue
+            if len(start_state) == 0:
+                print(str(start_state))
+                start_state.append(s)
+            else:
+                end_state.append(s)
+        # Strip square brackets
+        start_state[0] = start_state[0][1:]
+        start_state[-1] = start_state[-1][:-1]
+        end_state[0] = end_state[0][1]
+        end_state[-1] = end_state[-1][:-1]
+
+    elif len(sys.argv) == 3:
+        start_state = sys.argv[1]
+        end_state = sys.argv[2]
+    else:
+        start_state = input('Start state: ')
+        end_state = input('End state: ')
+    start_board = models.Board(start_state)
+    end_board = models.Board(end_state)
+    return start_board, end_board
 
 
 def main():
     start_state, goal_state = parse_input()
+    print('x')
+    print(str(start_state))
+
+main()
--- models/board.py
+++ models/board.py
@@ -1,27 +1,62 @@
-
+ROWS = 5
+COLS = 4
+SIZE = ROWS * COLS
 class Board():
     def __init__(self, board_state):
         self.state, self.empty_tile = calculate_state(board_state)
+    
+    def get_hash(self):
+        # very very lazy hash, shouldn't be repeated, and shouldn't take long to compare
+        return str(self.state)
+
+
+    def calc_num_moves(self, hueristic):
+        '''
+            Takes a hueristic, which is a function which takes a state
+
+            Returns the result of the hueristic
+        '''
+        return hueristic(self.state)
+    
+    def get_possible_moves(self):
+        # Move left, right, down, up respectively
+        moves = [self.empty_tile - 1, self.empty_tile + 1, 
+                 self.empty_tile + ROWS, self.empty_tile - ROWS]
+
+    def __str__(self):
+        result = ''
+
+        for i in range(len(self.state)):
+            
+            if (i % COLS) == 0:
+                result += '\n'
+                result += str(self.state[i])
+            else:
+                result += '\t'
+                result += str(self.state[i])
+                
+        return result
 
 def calculate_state(board_state):
+    print(str(board_state))
     if type(board_state) == str:
         if board_state[0] != '[' or board_state [-1] != ']':
             raise ValueError('Format invalid. The program expects there to be an opening and a closing square bracket for each state')
         values = [int(v) for v in board_state[1:-1].split(" ") if v != '']
 
-        if len(values) != 20:
-            raise ValueError(f'Format Invalid, the board expects exactly 20 tiles. It recieved {len(values)}.\n The values parsed were {values}')
-        if values.find(0) < 0:
+        if len(values) != SIZE:
+            raise ValueError(f'Format Invalid, the board expects exactly {SIZE} tiles. It recieved {len(values)}.\n The values parsed were {values}')
+        if values.index(0) < 0:
             raise ValueError('No empty tile found')
-        index_empty = values.find(0)
+        index_empty = values.index(0)
         values[index_empty] = None
         return values, index_empty
     elif type(board_state) == list:
         # Assume it has already been parsed
-        index_empty = board_state.find(0)
+        index_empty = board_state.index(0)
 
         if len(board_state) != 20:
-            raise ValueError(f'Format Invalid, the board expects exactly 20 tiles. It recieved {len(board_state)}.\n The values parsed were {board_state}')
+            raise ValueError(f'Format Invalid, the board expects exactly {SIZE} tiles. It recieved {len(board_state)}.\n The values parsed were {board_state}')
 
         if index_empty < 0:
             raise ValueError('No empty tile found')
